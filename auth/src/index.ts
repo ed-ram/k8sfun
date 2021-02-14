167c588b4f9216a911d89dc15b353794345de4f8
Parrot User <user@localhost.localdomain>
2021-02-14 14:21:26 +0000

    correct k8s yml identation

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,5 +5,9 @@ const app = express();
 
 app.use(json());
 
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('currentuser');
+})
+
 app.listen(3000, () => {console.log('listening... on 3000')});
 
